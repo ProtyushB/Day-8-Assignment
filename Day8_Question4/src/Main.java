ca3e78035d8111112a4795ad565420b98c97637d
ProtyushB <protyushprincebanerjee@gmail.com>
2022-05-29 22:28:39 +0530

    Day8

---
+++ Day8_Question4/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[3];
+
+        animals[0]=new Dog();
+        animals[1]=new Cat();
+        animals[2]=new Tiger();
+
+        for (int i=0;i<animals.length;i++){
+            animals[i].eat();
+            animals[i].walk();
+            animals[i].makeNoise();
+        }
+    }
+}
